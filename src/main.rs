d89ba567cbe3839bc980f241327a6b1d6c866f33
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2021-02-07 00:08:10 -0700

    mvp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
+use binance::account::*;
+use binance::api::*;
+use chrono::{DateTime, Duration, Utc};
+use config::*;
+use curl::easy::{Easy, List};
+use regex::Regex;
+use std::collections::HashMap;
+use std::str;
+use std::{thread, time};
+
+fn has_doge_string(response: &str) -> bool {
+    let re = Regex::new(r"(?i)[^@]doge").unwrap();
+    re.is_match(response)
+}
+
+fn place_order<F>(binance_api_key: &str, binance_secret_key: &str, order_size: F)
+where
+    F: Into<f64>,
+{
+    let account: Account = Binance::new(
+        Some(binance_api_key.to_string()),
+        Some(binance_secret_key.to_string()),
+    );
+    match account.market_buy("DOGEUSDT", order_size) {
+        Ok(r) => println!("Successful order: {:?}", r),
+        Err(e) => println!("Error placing order: {:?}", e),
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut config = config::Config::default();
+    config
+        .merge(config::File::with_name("Secrets"))
+        .unwrap()
+        .merge(config::Environment::with_prefix("ELON_DOGE"))
+        .unwrap();
+    let config = config.try_into::<HashMap<String, String>>().unwrap();
+    let binance_api_key: String = config.get("binance_api_key").unwrap().into();
+    let binance_secret_key: String = config.get("binance_secret_key").unwrap().into();
+    let twitter_bearer_token: String = config.get("twitter_bearer_token").unwrap().into();
+    let usdt_order_size: String = config.get("usdt_order_size").unwrap().into();
+    let usdt_order_size: f32 = usdt_order_size.parse::<f32>().unwrap();
+    loop {
+        let thirty_seconds_ago: DateTime<Utc> = Utc::now() - Duration::seconds(30);
+
+        let mut easy = Easy::new();
+        easy.url(&format!("https://api.twitter.com/2/tweets/search/recent?start_time={:?}&tweet.fields=created_at&query=(from:elonmusk)", thirty_seconds_ago)).unwrap();
+
+        let mut list = List::new();
+        let auth_header = format!("Authorization: Bearer {}", twitter_bearer_token);
+        list.append(&auth_header).unwrap();
+        easy.http_headers(list).unwrap();
+        let binance_api_key = binance_api_key.clone();
+        let binance_secret_key = binance_secret_key.clone();
+        easy.write_function(move |data| {
+            let result = str::from_utf8(data).unwrap().to_string();
+            println!("{}", result);
+            if has_doge_string(&result) {
+                place_order(&binance_api_key, &binance_secret_key, usdt_order_size);
+            }
+            Ok(data.len())
+        })
+        .unwrap();
+        easy.perform().unwrap();
+        let thirty_seconds = time::Duration::from_secs(30);
+        thread::sleep(thirty_seconds);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::has_doge_string;
+
+    #[test]
+    fn test_regex() {
+        assert_eq!(true, has_doge_string("buy Dogecoin"));
+        assert_eq!(false, has_doge_string("@dogeman"))
+    }
 }
